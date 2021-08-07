ea1a5062c666371e6d94d92e12f0917098440cd8
jeffj6123 <jeffrey.jarry@gmail.com>
2021-08-07 16:16:30 -0700

    progress.

--- index.ts
+++ index.ts
@@ -1,103 +1,228 @@
-export interface Node {
+const size = 500;
+const shapeCount = 10;
+
+const radius = 20;
+const FULL_RADIUS = Math.PI * 2;
+
+
+function distance(circle1Ref: IPoint, circle2Ref: IPoint, point: IPoint) {
+    let circle2 = circle2Ref;
+    let circle1 = circle1Ref;
+
+    if (circle2.x < circle1.x) {
+        circle2 = circle1Ref;
+        circle1 = circle2Ref;
+    }
+
+    const x2minsx1 = (circle2.x - circle1.x);
+    const y2minusy1 = (circle2.y - circle1.y);
+
+    const numerator = Math.abs(x2minsx1 * (circle1.y - point.y) -
+        (circle1.x - point.x) * y2minusy1)
+    const denominator = Math.sqrt( (x2minsx1 * x2minsx1) + (y2minusy1 * y2minusy1));
+
+    return numerator / denominator;
+}
+
+const boundCheck = (shape1: IPoint, shape2: IPoint, conflict: IPoint) => {
+
+    let copy1 = {...shape1};
+    let copy2 = {...shape2};
+    
+    if(copy1.x > copy2.x ) {
+        copy1.x -= radius;
+        copy2.x += radius;
+    }else {
+        copy2.x -= radius;
+        copy1.x += radius;
+    }
+
+    if(copy1.y > copy2.y ) {
+        copy1.y -= radius;
+        copy2.y += radius;
+    }else {
+        copy2.y -= radius;
+        copy1.y += radius;
+    }
+
+    const arr = [copy1, copy2, conflict];
+
+    arr.sort( (a,b) => a.x - b.x);
+    const xCheck = arr[1] === conflict;
+
+    arr.sort( (a,b) => a.y - b.y)
+    const yCheck = arr[1] === conflict;
+
+
+    return xCheck && yCheck;
+}
+
+const checkOverlap = (shape1: Circle, shape2: Circle) => {
+    const midPointDistance = getDistanceBetweenShapes(shape1.position, shape2.position);
+
+    return midPointDistance < (radius * 2)
+}
+
+const getDistanceBetweenShapes = (shape1: IPoint, shape2: IPoint) => {
+    return Math.sqrt(Math.pow((shape1.x) - (shape2.x), 2) + Math.pow((shape1.y) - (shape2.y), 2));
+}
+
+export interface IPoint {
     x: number;
     y: number;
-    id: string;
-}
-
-export interface Edge {
-    nodes: Node[];
-    id: string;
 }
 
 
-export interface INodePathFindingInfo {
-    parent: INodePathFindingInfo;
-    node: Node;
-    hCost: number;
-    gCost: number;
+export class BaseShape {
+
+    constructor(public id: string) {
+
+                }
+
+    draw(ctx: CanvasRenderingContext2D) {}
+
+    update() {}
 }
 
 
-export class AStarFinding {
 
-    private edgeMap: Record<string, Edge[]> = {};
-    constructor(private nodes: Node[],
-        private edges: Edge[]) {
-        this.nodes.forEach(node => {
-            this.edgeMap[node.id] = [];
-        })
-
-        edges.forEach(edge => {
-            edge.nodes.forEach(node => {
-                this.edgeMap[node.id].push(edge);
-            })
-        })
+class Circle extends BaseShape {
+    constructor(public position: IPoint,
+                public id: string) {
+        super(id);
     }
 
-    step() {
+    draw(ctx) {
+        // ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 / size * this.position.x) + ', ' +
+            Math.floor(255 / size * this.position.y) + ')';
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+
+        ctx.arc(this.position.x, this.position.y, radius, 0, FULL_RADIUS, true); // Outer circle
+
+        ctx.closePath();
+        ctx.stroke();
+
+
+        ctx.fillStyle = 'rgb(0, ' + Math.floor(255 / size * this.position.x) + ', ' +
+            Math.floor(255 / size * this.position.y) + ')';
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, radius / 2, 0, FULL_RADIUS, true);
+        ctx.fill();
+
+        ctx.fillText(this.id, this.position.x + radius , this.position.y);
+    }
+
+    update() {
+        // this.x = (this.x + this.xOffset) % size;
+        // this.y = (this.y + this.yoffSet ) % size;
+    }
+}
+
+class Vertice extends BaseShape {
+    constructor(public circle1: Circle,
+                public circle2: Circle,
+                public green = true) {
+       super(Vertice.getVerticeId(circle1, circle2));
+    }
+
+    update() {
 
     }
 
-    generatePath(startNode: Node, endNode: Node): INodePathFindingInfo[] {
-        const seen = new Set();
+    draw(ctx) {
+        ctx.strokeStyle = this.green ? 'green' : "red";
+        ctx.beginPath();
+        ctx.moveTo(this.circle1.position.x, this.circle1.position.y);
+        ctx.lineTo(this.circle2.position.x, this.circle2.position.y);
+        ctx.closePath();
+        ctx.stroke();
+    }
 
-        const available: INodePathFindingInfo[] = [];
+    public static getVerticeId(circle1: Circle, circle2: Circle): string {
+        return [circle1, circle2].map(c => c.id).sort().join("-");
+    }
+}
 
-        let currentNode: INodePathFindingInfo = {
-            parent: null,
-            node: startNode,
-            hCost: 0,
-            gCost: 0
-        };
 
-        while (currentNode && currentNode.node !== endNode) {
 
-            this.edgeMap[currentNode.node.id].forEach(edge => {
-                const newNode = edge.nodes.filter(n => currentNode.node.id === n.id)[0];
+export function draw() {
+    let canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    if (canvas.getContext) {
+        let ctx = canvas.getContext('2d');
 
-                if (!seen.has(newNode.id)) {
-                    const hCost = this.evaluateDistance(newNode, endNode);
-                    const gCost = this.evaluateDistance(currentNode.node, newNode);
-                    available.push({
-                        parent: currentNode,
-                        node: newNode,
-                        hCost,
-                        gCost
-                    });
+        let shapes: Circle[] = [];
+        let verts = [];
+        for (let i = 0; i < 20; i++) {
+            let x = Math.random() * size;
+            let y = Math.random() * size;
 
-                    seen.add(newNode.id);
+            const s = new Circle({y, x}, i.toString());
+            if (shapes.every(shape => !checkOverlap(shape, s))) {
+                shapes.push(s);
+            }
+        }
+
+        shapes.forEach(shape => {
+
+            const pairs = shapes.map((s, index) => {
+                return {
+                    distance: getDistanceBetweenShapes(shape.position, s.position),
+                    shape1: shape,
+                    shape2: s,
+                    id: index
+                }
+            }).filter((a) => a.shape1 !== a.shape2).sort((a, b) => a.distance - b.distance).slice(0, 4);
+
+
+            pairs.forEach(indice => {
+
+                let conflictShape = null;
+                shapes.forEach(s => {
+                    // console.log(distance(indice.shape1, indice.shape2, s), distance(indice.shape1, indice.shape2, s) > radius)
+                    // if (indice.shape1 === s || indice.shape2 === s) {
+                    //     return true;
+                    // }
+                    const d = distance(indice.shape1.position, indice.shape2.position, s.position);
+                    if( !(indice.shape1 === s || indice.shape2 === s) && 
+                        d < radius && boundCheck(indice.shape1.position, indice.shape2.position, s.position)) {
+                        conflictShape = s;
+                    }
+                })
+                // console.log(conflictShape)
+                // || !boundCheck(indice.shape1, indice.shape2, conflictShape.point)
+                if (!conflictShape) {
+                    // if(conflictShape && !boundCheck(indice.shape1, indice.shape2, conflictShape.point)) {
+                    //     console.log(distance(indice.shape1.position, indice.shape2.position, conflictShape.position),
+                    //     getDistanceBetweenShapes(conflictShape.position, indice.shape1.position),
+                    //     getDistanceBetweenShapes(conflictShape.position, indice.shape2.position), conflictShape, indice.shape1, indice.shape2) 
+                    // }
+                    verts.push(new Vertice(indice.shape2, indice.shape1));
+                } else {
+                    verts.push(new Vertice(indice.shape2, indice.shape1, false));
+                    // console.log(distance(indice.shape1, indice.shape2, conflictShape),
+                    // getDistanceBetweenShapes(conflictShape, indice.shape1),
+                    // getDistanceBetweenShapes(conflictShape, indice.shape2), conflictShape, indice.shape1, indice.shape2)
                 }
             })
 
-            currentNode = this.getLowestCostNode(available, true);
-        }
-
-        return [];
-    }
-
-    evaluateDistance(node: Node, goalNode: Node): number {
-        return Math.sqrt((node.x - goalNode.x) * (node.x - goalNode.x) - (node.y - goalNode.y) * (node.y - goalNode.y));
-    }
-
-    getLowestCostNode(nodes: INodePathFindingInfo[], remove = false): INodePathFindingInfo {
-        let node = nodes[0];
-        let index = 0;
-
-        nodes.forEach((info, i) => {
-            if (this.getTotalCost(info) < this.getTotalCost(node)) {
-                node = info;
-                index = i;
-            }
         })
 
-        if (remove) {
-            nodes.splice(index, 1);
-        }
+        let interval = setInterval(() => {
+            try {
+                ctx.clearRect(0, 0, size, size);
 
-        return node;
-    }
-
-    getTotalCost(node: INodePathFindingInfo): number {
-        return node.gCost + node.hCost;
+                shapes.concat(verts).forEach(shape => {
+                    shape.update()
+                    shape.draw(ctx)
+                })
+            } catch (e) {
+                console.error(e);
+                clearInterval(interval)
+            }
+        }, 1)
     }
 }
+
+draw();
---
+++ pathFinding.ts
@@ -0,0 +1,115 @@
+export interface Node {
+    x: number;
+    y: number;
+    id: string;
+}
+
+export interface Edge {
+    nodes: Node[];
+    id: string;
+}
+
+
+export interface INodePathFindingInfo {
+    parent: INodePathFindingInfo;
+    node: Node;
+    hCost: number;
+    gCost: number;
+}
+
+
+export class AStarFinding {
+
+    private edgeMap: Record<string, Edge[]> = {};
+    constructor(private nodes: Node[],
+        private edges: Edge[]) {
+        this.nodes.forEach(node => {
+            this.edgeMap[node.id] = [];
+        })
+
+        edges.forEach(edge => {
+            edge.nodes.forEach(node => {
+                this.edgeMap[node.id].push(edge);
+            })
+        })
+    }
+
+    step() {
+
+    }
+
+    generatePath(startNode: Node, endNode: Node): INodePathFindingInfo[] {
+        const seen = new Set();
+
+        const available: INodePathFindingInfo[] = [];
+
+        let currentNode: INodePathFindingInfo = {
+            parent: null,
+            node: startNode,
+            hCost: 0,
+            gCost: 0
+        };
+
+        while (currentNode && currentNode.node !== endNode) {
+
+            this.edgeMap[currentNode.node.id].forEach(edge => {
+                const newNode = edge.nodes.filter(n => currentNode.node.id === n.id)[0];
+
+                if (!seen.has(newNode.id)) {
+                    const hCost = this.evaluateDistance(newNode, endNode);
+                    const gCost = this.evaluateDistance(currentNode.node, newNode) + currentNode.gCost;
+                    available.push({
+                        parent: currentNode,
+                        node: newNode,
+                        hCost,
+                        gCost
+                    });
+
+                    seen.add(newNode.id);
+                }
+            })
+
+            currentNode = this.getLowestCostNode(available, true);
+        }
+
+        //TODO CHECK THAT ITS END NODE
+        return this.getPathAsList(currentNode);
+    }
+
+    evaluateDistance(node: Node, goalNode: Node): number {
+        return Math.sqrt((node.x - goalNode.x) * (node.x - goalNode.x) - (node.y - goalNode.y) * (node.y - goalNode.y));
+    }
+
+    getLowestCostNode(nodes: INodePathFindingInfo[], remove = false): INodePathFindingInfo {
+        let node = nodes[0];
+        let index = 0;
+
+        nodes.forEach((info, i) => {
+            if (this.getTotalCost(info) < this.getTotalCost(node)) {
+                node = info;
+                index = i;
+            }
+        })
+
+        if (remove) {
+            nodes.splice(index, 1);
+        }
+
+        return node;
+    }
+
+    getPathAsList(node: INodePathFindingInfo): INodePathFindingInfo[] {
+        let nodes = [];
+        
+        while(node.parent) {
+            nodes.push(node);
+            node = node.parent;
+        } 
+
+        return nodes;
+    }
+
+    getTotalCost(node: INodePathFindingInfo): number {
+        return node.gCost + node.hCost;
+    }
+}
