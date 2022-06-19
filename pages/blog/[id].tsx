3667e7bb84d38bd273a464ca25912521e2505a6f
jeff jarry <jeffrey.jarry@gmail.com>
2022-06-19 16:16:12 -0700

    wip.

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -11,27 +11,29 @@ export function Tag(props: TagProps){
     return (<span className="tag">{props.name}</span>)
 }
 
-
 //TODO SCORE UPDATER
 //MAKE AN UPVOTER THAT SCORES DEPENDING ON HOW MANY LAPS
-export default function Post(props: {content: Blog}) {
-    const Component = useMDXComponent(props.content.body.code)
-    const tags: ITag[] = props.content.tagList.map((tag: string) => ({ id: tag, display: tag }))
-    return (<div className="layout-container">
-        <h1 className="blog-title">{props.content.title}</h1>
-        <div className="underline"></div>
-        <TagsList tags={tags}></TagsList>
-        <div className="blog-content">
-            <Component></Component>
-        </div>
+export default function Post(props: {content: Blog, id: string}) {
+        const content = (props.content?.body?.code) || "var Component = function() {}; return Component";
 
-    </div>)
+        const Component = useMDXComponent(content)
+        const tags: ITag[] = props.content.tagList.map((tag: string) => ({ id: tag, display: tag }))
+        return (<div className="layout-container">
+            <h1 className="blog-title">{props.content.title}</h1>
+            <div className="underline"></div>
+            <TagsList tags={tags}></TagsList>
+            <div className="blog-content">
+                <Component></Component>
+            </div>
+    
+        </div>)
 }
 
 export async function getStaticProps(context: GetStaticPropsContext) {
-    const post = allBlogs.find(post => post.id === context.params.id) //.find((post) => post.title === (context.params.id + 'mdx'));
+    const post = allBlogs.find(post => post.id === context.params.id);
     return {
         props: {
+            id: context.params.id,
             content: post
         },
     }
@@ -40,6 +42,6 @@ export async function getStaticProps(context: GetStaticPropsContext) {
 export async function getStaticPaths() {
     return {
         paths: allBlogs.map(post => `/blog/${post.id}`),
-        fallback: true,
+        fallback: false,
     }
 }
